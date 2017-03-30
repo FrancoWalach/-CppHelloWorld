428b5f4b5522bb00cbeb4dbfe9f1a3fb90b264b2
FrancoWalach <francowalach@yahoo.com>
2017-03-30 10:59:57 -0300

    Add files via upload

---
+++ Hello world.cpp
@@ -0,0 +1,19 @@
+/* Hola Franco!  
+* Franco Leonel Walach  
+* 20150413  
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  cout << "Diga Hola" <<endl ;
+  int Hola ;
+  cin >> Hola ;
+  cout << "Hola Franco! " ;
+
+  }
+
